c3348054e116db1e898b6393df00c4a8883bdf76
Akshit Gaur <akshitgaur@proton.me>
2025-04-14 18:12:38 +0530

    Weights Import Working!!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use burn::{backend::Wgpu, module::Module, record::FullPrecisionSettings};
-use burn_import::pytorch::PyTorchFileRecorder;
+use burn_import::pytorch::{LoadArgs, PyTorchFileRecorder};
 use esrgan::model::RealESRGANConfig;
 use burn_ndarray;
 use burn::record::Recorder;
@@ -8,9 +8,13 @@ type Back = burn_ndarray::NdArray<f32>;
 
 fn main() {
     let device = Default::default();
+    let load_args = LoadArgs::new("./RealESRGAN_x4.pth".into())
+        .with_key_remap(r"^(.*)", "model.$1");
+
     let record = PyTorchFileRecorder::<FullPrecisionSettings>::default()
-        .load("./RealESRGAN_x4.pth".into(), &device)
-        .expect("Should load model!");
+        .load(load_args, &device)
+        .expect("Should decode state successfully");
+
     let model = RealESRGANConfig::new(3, 3, 4, 64, 23, 32).init::<Back>(&device).load_record(record);
 
     println!("{}", model);
